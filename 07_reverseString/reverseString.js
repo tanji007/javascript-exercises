20db097f7fca0c5c634285870ca4dfc1d334787b
tanji007 <yash.agrwal404@gmail.com>
2025-05-25 11:38:13 +0530

    Solved Exercise No (06, 07, 08, 09, 10 ,11)

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let arr = str.split("");
+    let str2 = arr.reverse().join("")
+    return str2
 };
-
+// Correct but could be more optimal
 // Do not edit below this line
 module.exports = reverseString;
+
--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,6 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...args) {
+    let arr2 = arr.filter((val)=> !args.includes(val))
+    return arr2
 };
 
 // Do not edit below this line
